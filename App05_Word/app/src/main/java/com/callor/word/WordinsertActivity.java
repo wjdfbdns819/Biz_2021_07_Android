2b2dff69df9e5a6be1e6317efdcb763ed3b23cb6
wjdfbdns819 <wjdfbdns819@wjdfbdns819.com>
2021-08-12 16:42:10 +0900

    2021-08-12

---
+++ App05_Word/app/src/main/java/com/callor/word/WordinsertActivity.java
@@ -0,0 +1,52 @@
+package com.callor.word;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.callor.word.databinding.ActivityWordinsertBinding;
+
+public class WordinsertActivity extends AppCompatActivity {
+
+    // Binding을 적용하기 위한 선언
+    ActivityWordinsertBinding wordBinding;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Binding을 적용하기 위한 생성(초기화)
+        wordBinding = ActivityWordinsertBinding.inflate(getLayoutInflater());
+
+        // Binding을 적용하여 Activity 화면 그리기
+        //setContentView(R.layout.activity_wordinsert);
+        setContentView(wordBinding.getRoot());
+        
+        // open한 Activity에게 return 하기 위한 intent 정보 추출
+        // open한 Activity에서 데이터를 보내면 그 데이터를 받는 용도로도 사용된다
+        Intent returnIntent = new Intent();
+
+        // 저장버튼을 클릭했을 때
+        wordBinding.btnSave.setOnClickListener(view->{
+            
+            // 두개의 입력박스에 입력한 값을 변수에 담기
+            String word = wordBinding.inputWord.getText().toString();
+            String korea = wordBinding.inputKorea.getText().toString();
+    
+            // WORD, KOREA 라는 변수를 각각 선언하고,
+            // word, korea에 담긴 값을 보내기 위하여 setting(putting)하기
+            returnIntent.putExtra("WORD", word);
+            returnIntent.putExtra("KOREA",korea);
+            
+            // 나를 open한 Activity 야 데이터가 준비되었으니(RESULT_OK)
+            // 데이터를 받아라
+            setResult(RESULT_OK,returnIntent);
+
+            // 새롭게 열린 Activity에서 자신을 닫는(끝내기) method
+            finish();
+
+        });
+    }
+}
---
+++ App05_Word/app/src/main/java/com/callor/word/repository/WordRepository.java
@@ -0,0 +1,48 @@
+package com.callor.word.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.callor.word.dao.WordDao;
+import com.callor.word.model.WordDTO;
+
+import java.util.List;
+
+/**
+ * WordDao와 WordDTO를 사용하여
+ *  DataBase 핸들링을 하는 중간 Service 클래스임
+ */
+public class WordRepository {
+
+    protected WordDao wordDao;
+    protected LiveData<List<WordDTO>> wordList;
+
+    /**
+     * Application Context를 매개변수로 갖는 생성자 필요
+     * 
+     */
+    public WordRepository(Application application) {
+    }
+
+    /**
+     * wordList를 단순히 return 만 하고 있는데
+     *  wordList 데이터를 가져오는 코드는 어디있는지?
+     * 
+     * 외부에서 repository.selectAll() method를 호출하면
+     *  내부에서 자동으로 Dao를 거쳐 DB에 Select를 수행하고,
+     *  그 결과를 return 한다
+     */
+    public LiveData<List<WordDTO>> selectAll() {
+
+        return wordList;
+    }
+
+    public void insert(WordDTO wordDTO) {
+        wordDao.insert(wordDTO);
+    }
+
+    public void deleteAll() {
+        wordDao.deleteAll();
+    }
+}
